6bac22e6a4b8d6026a24d7eba14e159f42e77800
markedwardmurray <markedwardmurray@gmail.com>
2015-10-13 15:23:48 -0400

    pods: update for xcode 7

--- Pods/Target Support Files/Pods-objc-Captain-Morgans-RelationshipsTests-OCHamcrest/Pods-objc-Captain-Morgans-RelationshipsTests-OCHamcrest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_objc_Captain_Morgans_RelationshipsTests_OCHamcrest : NSObject
-@end
-@implementation PodsDummy_Pods_objc_Captain_Morgans_RelationshipsTests_OCHamcrest
-@end
--- Pods/Target Support Files/Pods-objc-Captain-Morgans-RelationshipsTests-OHHTTPStubs/Pods-objc-Captain-Morgans-RelationshipsTests-OHHTTPStubs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_objc_Captain_Morgans_RelationshipsTests_OHHTTPStubs : NSObject
-@end
-@implementation PodsDummy_Pods_objc_Captain_Morgans_RelationshipsTests_OHHTTPStubs
-@end
